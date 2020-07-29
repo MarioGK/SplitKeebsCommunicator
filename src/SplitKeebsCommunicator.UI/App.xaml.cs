90a4244ee9eda7f959a02373042e8fda82bb890d
Mario Gabriell Karaziaki <mariogk01@gmail.com>
2020-07-29 01:34:56 -0300

    Renaming UI project

--- src/SplitKeebsCommunicator.UI/App.xaml.cs
+++ src/SplitKeebsCommunicator.UI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SplitKeebsCommunicator
+namespace SplitKeebsCommunicator.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
