85182e33aff4252135ae8c132ac1f197fc4b9b10
MarioGK <mariogk01@gmail.com>
2020-08-04 03:19:28 -0300

    Installing deps, and initiating the UI

--- Software/SplitKeebsCommunicator/SplitKeebsCommunicator/App.xaml.cs
+++ Software/SplitKeebsCommunicator/SplitKeebsCommunicator/App.xaml.cs
@@ -5,13 +5,24 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Notifications.Wpf.Core;
+using Prism.Ioc;
 
 namespace SplitKeebsCommunicator
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton(typeof(NotificationManager));
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
     }
 }
--- Software/SplitKeebsCommunicator/SplitKeebsCommunicator/MainWindow.xaml.cs
+++ Software/SplitKeebsCommunicator/SplitKeebsCommunicator/MainWindow.xaml.cs
@@ -12,17 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FontAwesome5;
+using Notifications.Wpf.Core;
 
 namespace SplitKeebsCommunicator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        public MainWindow()
+        private readonly NotificationManager _notificationManager;
+
+        public MainWindow(NotificationManager notificationManager)
         {
+            _notificationManager = notificationManager;
+            Icon = ImageAwesome.CreateImageSource(EFontAwesomeIcon.Regular_Keyboard, Brushes.White);
             InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            await _notificationManager.ShowAsync(new NotificationContent
+            {
+                Title = "Sample notification",
+                Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                Type = NotificationType.Information
+            });
         }
     }
 }
